11b9b3cd1f65d57ad025c0be6a2caaa047c75445
Ezeroth <37491048+Ezeroth@users.noreply.github.com>
2018-08-19 08:32:23 -0400

    BIG BRAIN
    Image correspond to brain. Big brain. MEGA BRAIN

--- Bot/commands/BIG BRAIN.js
+++ Bot/commands/BIG BRAIN.js
@@ -5,20 +5,60 @@ module.exports = {
     name: 'mega_brain',
     description: 'WHO HAS BIG BRAIN WINS',
     execute(client, message, args) {
-        if (message.author.id === ownerID) {
-            const brainPower = Math.floor(Math.random() * 1000000000)
+
+        const brainPower = Math.floor(Math.random() * 1000)
+        let brainImageGen = Math.floor(brainPower / 10)
+        let brainImage = 0
+        switch (brainImageGen) {
+            case 0:
+                brainImage = 'https://i.imgur.com/cb47Olf.png'
+                break;
+            case 1:
+                brainImage = 'https://i.imgur.com/OxvCSG9.png'
+                break;
+            case 2:
+                brainImage = 'https://i.imgur.com/gNsvZeM.png'
+                break;
+            case 3:
+                brainImage = 'https://i.imgur.com/4XqJPCp.png'
+                break;
+            case 4:
+                brainImage = 'https://i.imgur.com/Wo9Y1N5.png'
+                break;
+            case 5:
+                brainImage = 'https://i.imgur.com/Puc5CE2.png'
+                break;
+            case 6:
+                brainImage = 'https://i.imgur.com/TWUtqQw.png'
+                break;
+            case 7:
+                brainImage = 'https://i.imgur.com/pmN1uTt.png'
+                break;
+            case 8:
+                brainImage = 'https://i.imgur.com/UzPcFuo.png'
+                break;
+            case 9:
+                brainImage = 'https://i.imgur.com/XZdiGVl.jpg'
+                break;
+            case 10:
+                brainImage = 'https://i.imgur.com/WCIRn2d.png'
+                break;
+        }
+
+        if (!args) {
             const embed = new RichEmbed()
                 .setDescription(`${message.author.tag} has ${brainPower} brain powers`)
                 .setColor(goodColor)
-                .setImage('https://i.imgur.com/KWnGzEv.png')
+                .setImage()
             message.channel.send(embed)
-        } else {
+        } else if (message.mentions.members) {
+            const brained = message.mentions.members;
             const brainPower = Math.floor(Math.random() * 1000)
             const embed = new RichEmbed()
                 .setDescription(`${message.author.tag} has ${brainPower} brain powers`)
                 .setColor(goodColor)
-                .setImage('https://i.imgur.com/fBZAkdn.png')
+                .setImage()
             message.channel.send(embed)
         }
-    },
+    }
 };
