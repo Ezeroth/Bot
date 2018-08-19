ce9530dd44fb001f484af1622ab69a64fda8cef7
Ezeroth <37491048+Ezeroth@users.noreply.github.com>
2018-08-19 19:25:49 -0400

    Added Stats command

---
+++ Bot/commands/stats.js
@@ -0,0 +1,25 @@
+const { Client, RichEmbed } = require('discord.js');
+const { goodColor, badColor, version } = require('C:/Bot/Bot/config.json');
+
+module.exports = {
+    name: 'stats',
+    description: 'Displays information and stats about the bot',
+    execute(client, message, args) {
+        const serverName = message.guild.name
+        const ping = client.ping
+        const serverCount = client.guilds.size
+        const channelCount = client.channels.size
+        const userCount = client.users.size
+        const botAv = client.user.displayAvatarURL
+
+        const embed = new RichEmbed()
+            .setTitle('Bot Information')
+            .setColor(goodColor)
+            .addField('About', 'I am a general purpose discord bot that was created by Eze#8999. I am currently in my early testing stages, however I should be fully usable by the end of the month supposing I face no major issues. If you\'d like to have a commmand added, feel free to DM Eze#8999 or join the support server(linked at the bottom)')
+            .addField('Bot ID', '478677061607620609')
+            .addField('Server Name', serverName)
+            .addField('Presence', `${serverCount} servers.\n${channelCount} channels.\n${userCount} users.`)
+            .setThumbnail(botAv)
+        message.channel.send(embed)
+    },
+};
