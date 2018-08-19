93f870f5613b4ce358fd6c06b175e6324f505078
Ezeroth <37491048+Ezeroth@users.noreply.github.com>
2018-08-19 14:08:53 -0400

    Big brain = big brain
    Added corresponding images depending on what your brain power is.

--- Bot/commands/BIG BRAIN.js
+++ Bot/commands/BIG BRAIN.js
@@ -7,57 +7,57 @@ module.exports = {
     execute(client, message, args) {
 
         const brainPower = Math.floor(Math.random() * 1000)
-        let brainImageGen = Math.floor(brainPower / 10)
-        let brainImage = 0
+        let brainImageGen = Math.floor(brainPower / 100)
+        let brainImage = 'https://i.imgur.com/WCIRn2d.png'
         switch (brainImageGen) {
             case 0:
-                brainImage = 'https://i.imgur.com/cb47Olf.png'
+                brainImage = "https://i.imgur.com/cb47Olf.png";
                 break;
             case 1:
-                brainImage = 'https://i.imgur.com/OxvCSG9.png'
+                brainImage = "https://i.imgur.com/OxvCSG9.png";
                 break;
             case 2:
-                brainImage = 'https://i.imgur.com/gNsvZeM.png'
+                brainImage = "https://i.imgur.com/gNsvZeM.png";
                 break;
             case 3:
-                brainImage = 'https://i.imgur.com/4XqJPCp.png'
+                brainImage = "https://i.imgur.com/4XqJPCp.png";
                 break;
             case 4:
-                brainImage = 'https://i.imgur.com/Wo9Y1N5.png'
+                brainImage = "https://i.imgur.com/Wo9Y1N5.png";
                 break;
             case 5:
-                brainImage = 'https://i.imgur.com/Puc5CE2.png'
+                brainImage = "https://i.imgur.com/Puc5CE2.png";
                 break;
             case 6:
-                brainImage = 'https://i.imgur.com/TWUtqQw.png'
+                brainImage = "https://i.imgur.com/TWUtqQw.png";
                 break;
             case 7:
-                brainImage = 'https://i.imgur.com/pmN1uTt.png'
+                brainImage = "https://i.imgur.com/pmN1uTt.png";
                 break;
             case 8:
-                brainImage = 'https://i.imgur.com/UzPcFuo.png'
+                brainImage = "https://i.imgur.com/UzPcFuo.png";
                 break;
             case 9:
-                brainImage = 'https://i.imgur.com/XZdiGVl.jpg'
+                brainImage = "https://i.imgur.com/XZdiGVl.jpg";
                 break;
             case 10:
-                brainImage = 'https://i.imgur.com/WCIRn2d.png'
+                brainImage = "https://i.imgur.com/WCIRn2d.png";
                 break;
         }
+
 
         if (!args) {
             const embed = new RichEmbed()
                 .setDescription(`${message.author.tag} has ${brainPower} brain powers`)
                 .setColor(goodColor)
-                .setImage()
+                .setImage(brainImage)
             message.channel.send(embed)
         } else if (message.mentions.members) {
             const brained = message.mentions.members;
-            const brainPower = Math.floor(Math.random() * 1000)
             const embed = new RichEmbed()
                 .setDescription(`${message.author.tag} has ${brainPower} brain powers`)
                 .setColor(goodColor)
-                .setImage()
+                .setImage(brainImage)
             message.channel.send(embed)
         }
     }
