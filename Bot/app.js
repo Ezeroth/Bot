05d536c35b379d756dd9a62e413e2d46e1803dcc
Ezeroth <37491048+Ezeroth@users.noreply.github.com>
2018-08-23 01:54:10 -0400

    irrelevant

--- Bot/app.js
+++ Bot/app.js
@@ -103,15 +103,32 @@ client.on("message", message => {
 })
 
 client.on("message", message => {
-    const role = '479603451006025729'
+    if (client.user.id === '478677061607620609') {
 
-    if (message.channel.id === '478679577175261185' && message.content === 'agree') {
-        message.member.addRole(role)
-        const embed = new RichEmbed()
-            .setDescription('Successfully Added Aquanatics Role')
-            .setColor(goodColor)
+        if (message.channel.id === '478679577175261185' && message.content === 'agree') {
+            const role = '479603451006025729'
+            message.member.addRole(role)
+            const embed = new RichEmbed()
+                .setDescription('Successfully Added Aquanatics Role')
+                .setColor(goodColor)
+        }
+    } else if (message.guild.id === '446804554248355842' && message.content.startsWith("member")) {
+        if (message.member.hasPermission("ADMINISTRATOR")) {
+            const icon = message.guild.iconURL
+
+            message.mentions.members.first().addRole('448980272344727582')
+            const embed = new RichEmbed()
+                .setDescription(`Welcome ${message.mentions.users.first()} to the Heavens Wrath King's Raid guild. If you have any questions or are ever in need of advice, feel free to ask in <#446804554827431937>! `)
+                .setColor(0x15D6EF)
+                .setImage(icon)
+
+            message.channel.send(embed)
+        } else {
+            message.channel.send('This is an officer only command.')
+        }
     }
 })
+
 
 client.on("message", message => {
     const args = message.content.split(" ").slice(1);
