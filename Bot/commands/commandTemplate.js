d63cdb86f7decf55e6bf139aa0149c1a42dac070
Ezeroth <37491048+Ezeroth@users.noreply.github.com>
2018-08-26 19:47:48 -0400

    Markdown is hot

--- Bot/commands/commandTemplate.js
+++ Bot/commands/commandTemplate.js
@@ -1,10 +1,31 @@
 const { Client, RichEmbed } = require('discord.js');
 const { goodColor, badColor } = require('./config.json');
+fs = require('fs');
+
+const commandFiles = fs.readdirSync('./').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./${file}`);
+    client.commands.set(command.name, command);
+}
 
 module.exports = {
-    name: 'COMMANDNAME',
-    description: 'COMMANDDESCRIPTION',
+    name: 'commands',
+    description: 'Returns a list with all the commands.',
     execute(client, message, args) {
-        console.log('this is the template')
+        fs.readdir('./', (err, files) => {
+
+            const commands = command
+
+            const description =
+                files.forEach(function (file) {
+                    commands.name += file + '\n';
+                });
+            const embed = new RichEmbed()
+                .setTitle('All Commands')
+                .setColor(goodColor)
+                .setDescription(description)
+            message.channel.send(embed);
+        });
     },
 };
